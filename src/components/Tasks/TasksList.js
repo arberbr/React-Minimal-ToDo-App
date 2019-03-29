a5d90a18c5be1ade6265ccb95921da242f72fde8
Arber Braja <admin@arbraja.com>
2019-03-29 17:24:53 +0100

    Solving bug of creating Tasks with empty Task description

--- src/components/Tasks/TasksList.js
+++ src/components/Tasks/TasksList.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import Tasks from './Tasks';
 import AddTask from './AddTask';
@@ -6,14 +6,16 @@ import TasksListHeader from './TasksListHeader';
 import TasksListFooter from './TasksListFooter';
 
 class TasksList extends Component {
-	
 	state = {
 		task: '',
 		tasks: []
 	};
-	
+
 	addTaskHandler = () => {
-		if (this.state.task.length === 0) alert('Please enter some text to describe the Task!');
+		if (this.state.task.length <= 3) {
+			alert('Please enter some text to describe the Task!');
+			return;
+		}
 		const newTask = {
 			id: Date.now(),
 			item: this.state.task,
@@ -23,46 +25,54 @@ class TasksList extends Component {
 		this.setState({
 			tasks: allTasks
 		});
- 	};
-	
-	onFormSubmit = (event) => {
+	};
+
+	onFormSubmit = event => {
 		event.preventDefault();
 	};
-	
-	onChangeHandler = (event) => {
+
+	onChangeHandler = event => {
 		this.setState({
 			task: event.target.value
 		});
 	};
-	
-	changeTaskStatus = (taskId) => {
+
+	changeTaskStatus = taskId => {
 		const newTasks = this.state.tasks.map(task => {
-			if(task.id === taskId) task = { ...task, active: !task.active };
+			if (task.id === taskId) task = { ...task, active: !task.active };
 			return task;
 		});
 		this.setState({
 			tasks: newTasks
 		});
 	};
-	
-	deleteSingleTask = (taskId) => {
+
+	deleteSingleTask = taskId => {
 		const chosenTask = this.state.tasks.filter(task => {
 			return task.id !== taskId;
 		});
 		this.setState({
 			tasks: chosenTask
-		})
+		});
 	};
-	
+
 	render() {
 		return (
 			<div className="taskslist-box">
-				<TasksListHeader/>
-				<AddTask addTask={() => this.addTaskHandler()} changed={(event)=>this.onChangeHandler(event)} formSubmit={(event)=> this.onFormSubmit(event)} />
-				<Tasks tasksList={this.state.tasks} changeTaskStatus={(taskId) => this.changeTaskStatus(taskId)} deleteSingleTask={(taskId) => this.deleteSingleTask(taskId)} />
-				<TasksListFooter tasksList={this.state.tasks}/>
+				<TasksListHeader />
+				<AddTask
+					addTask={() => this.addTaskHandler()}
+					changed={event => this.onChangeHandler(event)}
+					formSubmit={event => this.onFormSubmit(event)}
+				/>
+				<Tasks
+					tasksList={this.state.tasks}
+					changeTaskStatus={taskId => this.changeTaskStatus(taskId)}
+					deleteSingleTask={taskId => this.deleteSingleTask(taskId)}
+				/>
+				<TasksListFooter tasksList={this.state.tasks} />
 			</div>
-		)
+		);
 	}
 }
 
